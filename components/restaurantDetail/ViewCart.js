806a061bd176879b084822fa155b63982131937a
Luke Hayes <lukehayesa@gmail.com>
2023-03-13 17:38:02 -0400

    add redux

--- components/restaurantDetail/ViewCart.js
+++ components/restaurantDetail/ViewCart.js
@@ -1,15 +1,32 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function ViewCart() {
+
+  const items = useSelector((state) => state.cartReducer.selectedItems.items);
+  const total = items
+        .map((item) => Number(item.price.replace('$', '')))
+        .reduce((prev, curr) => prev + curr, 0);
+
+  const totalUSD = total.toLocaleString('en', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  console.log(totalUSD, 'MONEY MONEY')
+
   return (
+    <>
+    {/* if total is present, show cart button */}
+    {total ? (
     <View style={{
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
     position: 'absolute',
-    bottom: 200,
+    bottom: 180,
     zIndex: 999,
     }}
     >
@@ -22,16 +39,26 @@ export default function ViewCart() {
         <TouchableOpacity style={{
             marginTop: 20, 
             backgroundColor: 'black',
-            alignItems: 'center',
-            padding: 13,
+            flexDirection: 'row',
+            justifyContent: 'flex-end',
+            padding: 15,
             borderRadius: 30,
             width: 300,
             position: 'relative',
             }}
             >
-      <Text style={{color: 'white', fontSize: 20}}>View Cart</Text>
+      <Text style={{color: 'white', fontSize: 20, marginRight: 30}}>
+        View Cart
+      </Text>
+      <Text style={{color: 'white', fontSize: 20}}>
+        {totalUSD}
+      </Text>
       </TouchableOpacity>
     </View>
     </View>
+     ) : (
+    <></>
+    )}
+  </>
   )
 }
